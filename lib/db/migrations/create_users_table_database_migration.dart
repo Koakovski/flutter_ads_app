2f6b9d3ece50ce237249031609d7920a553ac10c
koakovski <hkoakovskip@gmail.com>
2024-11-24 10:59:16 -0300

    feat: make email column unique

--- lib/db/migrations/create_users_table_database_migration.dart
+++ lib/db/migrations/create_users_table_database_migration.dart
@@ -11,7 +11,7 @@ class CreateUsersTableDatabaseMigration implements DatabaseMigration {
       CREATE TABLE IF NOT EXISTS users (
         id TEXT PRIMARY KEY,
         name TEXT NOT NULL,
-        email TEXT NOT NULL
+        email TEXT NOT NULL UNIQUE
       );
     ''');
   }
