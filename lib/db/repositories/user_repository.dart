78d1a57994264de2583d2df35d0be9f67b019b77
koakovski <hkoakovskip@gmail.com>
2024-11-24 11:14:31 -0300

    feat: add findAll method to UserRepository

--- lib/db/repositories/user_repository.dart
+++ lib/db/repositories/user_repository.dart
@@ -6,6 +6,19 @@ import 'package:sqflite/sqflite.dart';
 class UserRepository {
   final Database _dbService = DatabaseService().db;
 
+  Future<List<User>> findAll() async {
+    final List<Map> persistenceCategories = await _dbService.rawQuery('''
+      SELECT 
+        *
+      FROM 
+        users
+    ''');
+
+    return persistenceCategories
+        .map((map) => UserMapper.toDomain(map))
+        .toList();
+  }
+
   Future<void> create(User user) async {
     await _dbService.insert('users', UserMapper.toPersistence(user));
   }
