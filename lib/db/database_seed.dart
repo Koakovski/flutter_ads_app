e7138894eee48be8069286e26bd8b9d35e281532
koakovski <hkoakovskip@gmail.com>
2024-11-24 11:29:49 -0300

    feat: add DatabaseSeed

---
+++ lib/db/database_seed.dart
@@ -0,0 +1,31 @@
+import 'package:ads_app/db/repositories/advertisement_repository.dart';
+import 'package:ads_app/db/repositories/category_repository.dart';
+import 'package:ads_app/db/repositories/user_repository.dart';
+import 'package:ads_app/mocks/advertisements_mocks.dart';
+import 'package:ads_app/mocks/category_mocks.dart';
+import 'package:ads_app/mocks/user_mocks.dart';
+
+class DatabaseSeed {
+  static Future<void> execute() async {
+    await Future.wait([_seedUsers(), _seedCategories()]);
+    await _seedAdvertisments();
+  }
+
+  static Future<void> _seedUsers() async {
+    for (var user in usersMock) {
+      await UserRepository().upsert(user);
+    }
+  }
+
+  static Future<void> _seedCategories() async {
+    for (var category in categoriesMock) {
+      await CategoryRepository().upsert(category);
+    }
+  }
+
+  static Future<void> _seedAdvertisments() async {
+    for (var adivertisement in advertisementsMock) {
+      await AdvertisementRepository().upsert(adivertisement);
+    }
+  }
+}
